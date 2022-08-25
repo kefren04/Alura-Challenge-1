bf70d78d2bbb9ead7ff7a527b06b9ccf3ee21ac6
kefren04 <111463674+kefren04@users.noreply.github.com>
2022-08-25 16:23:00 -0400

    Encriptador para el challenge
    Challenge 1_G3

---
+++ script.js
@@ -0,0 +1,55 @@
+
+
+function encriptar(){
+    
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    var txtCifrado = texto.replace(/e/igm,"enter");
+    var txtCifrado = txtCifrado.replace(/o/igm,"ober");
+    var txtCifrado = txtCifrado.replace(/i/igm,"imes");
+    var txtCifrado = txtCifrado.replace(/a/igm,"ai");
+    var txtCifrado = txtCifrado.replace(/u/igm,"ufat");
+
+    document.getElementById("munheco").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+
+}
+
+
+
+function desencriptar(){
+    
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    var txtCifrado = texto.replace(/enter/igm,"e");
+    var txtCifrado = txtCifrado.replace(/ober/igm,"o");
+    var txtCifrado = txtCifrado.replace(/imes/igm,"i");
+    var txtCifrado = txtCifrado.replace(/ai/igm,"a");
+    var txtCifrado = txtCifrado.replace(/ufat/igm,"u");
+
+    document.getElementById("munheco").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+
+}
+
+
+
+function copy(){
+    var contenido = document.getElementById("texto2");
+    contenido.select();
+    document.execCommand("copy");
+    alert ("Texto copiado");
+    inputTexto.focus();
+    inputTexto.value = "";
+
+} 
+
+
+
+
+
+
